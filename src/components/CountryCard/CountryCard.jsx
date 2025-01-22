dc111313e88c69c261f895854a43c637fc9b267a
PontusBengtsson <pontus.bengtsson@live.com>
2025-01-23 00:07:29 +0100

    Skeleton Loaders V3

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,146 +1,102 @@
 import React from 'react';
 import { Box, Typography, Button } from '@mui/material';
-import { useTheme } from '@mui/material/styles'; // Importera temat
+import { useTheme } from '@mui/material/styles';
 
 const CountryCard = ({ country, handleBorderClick, handleBackClick }) => {
-  const theme = useTheme(); // Hämta aktuellt tema
-  const nativeName = country.name.nativeName ? Object.values(country.name.nativeName)[0].common : 'N/A';
-  const topLevelDomain = country.tld ? country.tld.join(', ') : 'N/A';
+    const theme = useTheme();
+    const nativeName = country.name.nativeName ? Object.values(country.name.nativeName)[0].common : 'N/A';
+    const topLevelDomain = country.tld ? country.tld.join(', ') : 'N/A';
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: '32px',
-      }}
-    >
-      <Box sx={{ width: '1150px' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-          <Box
+    return (
+        <Box
             sx={{
-              padding: '64px 32px 0px 0px',
-              width: '50%',
-              height: 'auto',
-            }}
-          >
-            <img
-              src={country.flags.png}
-              alt={`${country.name.common} flag`}
-              style={{ borderRadius: '8px', width: '100%' }}
-            />
-          </Box>
-          <Box
-            sx={{
-              flex: 1,
-              width: '50%',
-              height: 'auto',
-              display: 'flex',
-              flexDirection: 'column',
-              padding: '64px 0px 0px 32px',
-            }}
-          >
-            <Typography variant="h3" sx={{ marginBottom: '16px' }}>
-              {country.name.common}
-            </Typography>
-            <Box
-              sx={{
                 display: 'flex',
-                flexDirection: 'row',
-                marginBottom: '40px',
-              }}
-            >
-              <Box sx={{ width: '50%' }}>
-                <DetailItem label="Population" value={country.population.toLocaleString()} />
-                <DetailItem label="Region" value={country.region} />
-                <DetailItem label="Capital" value={country.capital ? country.capital[0] : 'N/A'} />
-                <DetailItem label="Native Name" value={nativeName} />
-              </Box>
-              <Box sx={{ width: '50%' }}>
-                <DetailItem label="Top Level Domain" value={topLevelDomain} />
-                <DetailItem
-                  label="Currencies"
-                  value={Object.values(country.currencies)
-                    .map((c) => c.name)
-                    .join(', ')}
-                />
-                <DetailItem label="Languages" value={Object.values(country.languages).join(', ')} />
-              </Box>
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: '32px',
+            }}
+        >
+            <Box sx={{ width: '1150px' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                    <Box sx={{ padding: '64px 32px 0px 0px', width: '50%' }}>
+                        <img
+                            src={country.flags.png}
+                            alt={`${country.name.common} flag`}
+                            style={{ borderRadius: '8px', width: '100%' }}
+                        />
+                    </Box>
+                    <Box sx={{ flex: 1, width: '50%', padding: '64px 0px 0px 32px' }}>
+                        <Typography variant="h3" sx={{ marginBottom: '16px' }}>
+                            {country.name.common}
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', marginBottom: '40px' }}>
+                            <Box sx={{ width: '50%' }}>
+                                <DetailItem label="Population" value={country.population.toLocaleString()} />
+                                <DetailItem label="Region" value={country.region} />
+                                <DetailItem label="Capital" value={country.capital ? country.capital[0] : 'N/A'} />
+                                <DetailItem label="Native Name" value={nativeName} />
+                            </Box>
+                            <Box sx={{ width: '50%' }}>
+                                <DetailItem label="Top Level Domain" value={topLevelDomain} />
+                                <DetailItem
+                                    label="Currencies"
+                                    value={Object.values(country.currencies)
+                                        .map((c) => c.name)
+                                        .join(', ')}
+                                />
+                                <DetailItem label="Languages" value={Object.values(country.languages).join(', ')} />
+                            </Box>
+                        </Box>
+                        <BorderCountries borders={country.borders} handleBorderClick={handleBorderClick} />
+                    </Box>
+                </Box>
             </Box>
-            <BorderCountries borders={country.borders} handleBorderClick={handleBorderClick} />
-          </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 const DetailItem = ({ label, value }) => (
-  <Box sx={{ display: 'flex', gap: '5px', mb: 1 }}>
-    <Typography variant="body1">
-      <strong>{label}:</strong>
-    </Typography>
-    <Typography variant="body1">{value}</Typography>
-  </Box>
+    <Box sx={{ display: 'flex', gap: '5px', mb: 1 }}>
+        <Typography variant="body1">
+            <strong>{label}:</strong>
+        </Typography>
+        <Typography variant="body1">{value}</Typography>
+    </Box>
 );
 
 const BorderCountries = ({ borders, handleBorderClick }) => {
-  const theme = useTheme(); // Hämta temat
+    const theme = useTheme();
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: '16px',
-        overflowX: 'auto',
-        whiteSpace: 'nowrap',
-      }}
-    >
-      <Typography
-        variant="body2"
-        sx={{
-          marginRight: '8px',
-        }}
-      >
-        <strong>Border Countries:</strong>
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          overflowX: 'auto',
-          whiteSpace: 'nowrap',
-          flexWrap: 'nowrap',
-        }}
-      >
-        {borders && borders.length > 0 ? (
-          borders.map((borderCountryCode) => (
-            <Button
-              key={borderCountryCode}
-              onClick={() => handleBorderClick(borderCountryCode)}
-              sx={{
-                backgroundColor: theme.palette.background.paper, // Grundfärg
-                borderRadius: '16px',
-                color: theme.palette.text.primary, // Textfärg
-                marginRight: '8px',
-                whiteSpace: 'nowrap',
-                flexShrink: 0,
-                '&:hover': {
-                  backgroundColor: 'background.paper', // Hover-effekt
-                },
-              }}
-            >
-              {borderCountryCode}
-            </Button>
-          ))
-        ) : (
-          <Typography variant="body2">No border countries</Typography>
-        )}
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '16px' }}>
+            <Typography variant="body2" sx={{ marginRight: '8px' }}>
+                <strong>Border Countries:</strong>
+            </Typography>
+            <Box sx={{ display: 'flex', gap: '8px' }}>
+                {borders && borders.length > 0 ? (
+                    borders.map((borderCountryCode) => (
+                        <Button
+                            key={borderCountryCode}
+                            onClick={() => handleBorderClick(borderCountryCode)}
+                            sx={{
+                                backgroundColor: theme.palette.background.paper,
+                                borderRadius: '16px',
+                                color: theme.palette.text.primary,
+                                '&:hover': {
+                                    backgroundColor: theme.palette.action.hover,
+                                },
+                            }}
+                        >
+                            {borderCountryCode}
+                        </Button>
+                    ))
+                ) : (
+                    <Typography variant="body2">No border countries</Typography>
+                )}
+            </Box>
+        </Box>
+    );
 };
 
 export default CountryCard;
--- src/pages/pages/CountryPage.jsx
+++ src/pages/pages/CountryPage.jsx
@@ -4,165 +4,176 @@ import { Box, Typography, Button, Skeleton } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import CountryCard from '../../components/CountryCard/CountryCard';
 
+const SkeletonBox = ({ width, height, sx = {} }) => (
+  <Skeleton variant="rectangular" width={width} height={height} sx={{ borderRadius: '4px', ...sx }} />
+);
+
 const CountryPage = () => {
-	const { countryCode } = useParams();
-	const navigate = useNavigate();
-	const [country, setCountry] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const theme = useTheme();
+  const { countryCode } = useParams();
+  const navigate = useNavigate();
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const theme = useTheme(); // Använd useTheme för att få åtkomst till temat
 
-	useEffect(() => {
-		const fetchCountry = async () => {
-			setLoading(true); // Start loading when fetching begins
-			try {
-				console.log('Fetching country with countryCode: ', countryCode); // Debugging
-				const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
-				if (!response.ok) {
-					throw new Error('Failed to fetch country data');
-				}
-				const data = await response.json();
-				setCountry(data[0]); // Set country data
-			} catch (error) {
-				console.error(error); // Log error if fetch fails
-			} finally {
-				setLoading(false); // Set loading to false after fetch completes
-			}
-		};
+  // Logga temats läge för felsökning
+  useEffect(() => {
+    console.log('Current theme mode:', theme.palette.mode);
+  }, [theme]);
 
-		fetchCountry();
-	}, [countryCode]);
+  useEffect(() => {
+    const fetchCountry = async () => {
+      setLoading(true);
+      try {
+        console.log('Fetching country with countryCode: ', countryCode);
+        const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch country data');
+        }
+        const data = await response.json();
+        setCountry(data[0]);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-	const handleBackClick = () => {
-		navigate('/'); // Navigate back to home
-	};
+    fetchCountry();
+  }, [countryCode]);
 
-	if (loading) {
-		return (
-			<Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-				<Box sx={{ marginTop: '32px', width: '1150px' }}>
-					<Skeleton variant="text" width="20%" sx={{ marginBottom: '16px', borderRadius: '4px' }} />
-					<Box sx={{ display: 'flex', flexDirection: 'row', marginBottom: '10px' }}>
-						<Skeleton
-							variant="rectangular"
-							width="50%"
-							height={300}
-							sx={{ marginRight: '32px', borderRadius: '8px' }}
-						/>
-						<Box sx={{ display: 'flex', flexDirection: 'column',width:"50%" }}>
-							<Skeleton
-								variant="text"
-								width="50%"
-								sx={{ marginBottom: '8px', borderRadius: '4px' }} //rubrik
-							/>
-							<Box sx={{ display: 'flex', flexDirection: 'column',width:"50%" }}>
-								<Box sx={{ display: 'flex', flexDirection: 'row',width:"100%"  }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column',width: '50%'  }}>
-                <Skeleton
-								variant="text"
-							
-								sx={{  borderRadius: '4px', marginleft:'25px' }}
-							/>
-                    <Skeleton
-								variant="text"
-							
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
-                    <Skeleton
-								variant="text"
-								
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
-                    <Skeleton
-								variant="text"
-								
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
+  const handleBorderCountryClick = (borderCountryCode) => {
+    navigate(`/country/${borderCountryCode}`);
+  };
+
+  if (loading) {
+    return (
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Box sx={{ marginTop: '32px', width: '1150px' }}>
+          <SkeletonBox width="20%" sx={{ padding: '17px', marginBottom: '96px' }} />
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <SkeletonBox width={543} height={389} sx={{ marginRight: '64px' }} />
+            <Box sx={{ width: '543px', flexDirection: 'column' }}>
+              <SkeletonBox width="50%" height={56} sx={{ marginBottom: '16px' }} />
+              <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', marginBottom: '30px' }}>
+                {[...Array(4)].map((_, i) => (
+                  <Box
+                    key={i}
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      width: '100%',
+                      gap: '10px',
+                      marginLeft: i === 0 ? 0 : '20px'
+                    }}
+                  >
+                    {[...Array(4 - i)].map((_, j) => (
+                      <SkeletonBox key={j} />
+                    ))}
                   </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'column',marginLeft:'20px', width: '50%' }}>
-                  <Skeleton
-								variant="text"
-								
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
-                        <Skeleton
-								variant="text"
-								
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
-                        <Skeleton
-								variant="text"
-								
-								sx={{  borderRadius: '4px', marginright:'25px' }}
-							/>
-                  
-								
-                  </Box>
-								</Box>
-							</Box>
-							<Box sx={{ width: '50px', display: 'flex', flexDirection: 'row' }}></Box>
-							<Skeleton
-								variant="text"
-								width="40%"
-								sx={{ marginBottom: '8px', borderRadius: '4px', padding: '20px' }}
-							/>
-						</Box>
-					</Box>
-				</Box>
-			</Box>
-		);
-	}
+                ))}
+              </Box>
+              <SkeletonBox width="100%" sx={{ padding: '20px', marginTop: '50px' }} />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
 
-	if (!country) {
-		return (
-			<Box sx={{ padding: '16px', marginTop: '32px', textAlign: 'center' }}>
-				<Typography variant="h6">Failed to load country data. Please try again later.</Typography>
-			</Box>
-		);
-	}
+  if (!country) {
+    return (
+      <Box sx={{ padding: '16px', marginTop: '32px', textAlign: 'center' }}>
+        <Typography variant="h6">Failed to load country data. Please try again later.</Typography>
+      </Box>
+    );
+  }
 
-	return (
-		<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '32px' }}>
-			<Box sx={{ width: '1150px' }}>
-				<Button
-					variant="contained"
-					onClick={handleBackClick}
-					startIcon={
-						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32.242 18.693">
-							<g transform="translate(-202.723 -137.717)">
-								<path
-									d="M16475.775,1222.011l-8.639,8.639,8.639,8.639"
-									transform="translate(-16263 -1083.587)"
-									fill="none"
-									stroke={theme.palette.text.primary}
-									strokeWidth="2"
-								/>
-								<line
-									x1="30.57"
-									transform="translate(204.395 147.155)"
-									fill="none"
-									stroke={theme.palette.text.primary}
-									strokeWidth="2"
-								/>
-							</g>
-						</svg>
-					}
-					sx={{
-						color: theme.palette.text.primary,
-						backgroundColor: theme.palette.background.default,
-						boxShadow: 'none',
-						padding: '5px',
-						textTransform: 'none',
-						'&:hover': {
-							backgroundColor: theme.palette.action.hover
-						}
-					}}
-				>
-					BACK
-				</Button>
-				<CountryCard country={country} handleBackClick={handleBackClick} />
-			</Box>
-		</Box>
-	);
+  // Kontrollera om mörkt läge är aktivt
+  const isDarkMode = theme.palette.mode === 'dark';
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '32px' }}>
+      <Box sx={{ width: '1150px' }}>
+      <Button
+  variant="contained"
+  onClick={handleBackClick}
+  sx={{
+    color: theme.palette.text.primary,
+    backgroundColor: theme.palette.background.default,
+    boxShadow: 'none',
+    padding: '5px',
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover
+    },
+    display: 'flex',  // Gör så att innehållet (ikonen och texten) kan justeras horisontellt
+    alignItems: 'center' // Centrerar innehållet vertikalt i knappen
+  }}
+>
+  {/* Använd isDarkMode för att byta färg på pilen */}
+  {isDarkMode ? (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32.242" height="18.693" viewBox="0 0 32.242 18.693">
+      <g id="Group_1069" data-name="Group 1069" transform="translate(-202.723 -137.717)">
+        <path
+          id="Path_278"
+          data-name="Path 278"
+          d="M16475.775,1222.011l-8.639,8.639,8.639,8.639"
+          transform="translate(-16263 -1083.587)"
+          fill="none"
+          stroke={theme.palette.text.primary}
+          stroke-width="2"
+        />
+        <line
+          id="Line_68"
+          data-name="Line 68"
+          x1="30.57"
+          transform="translate(204.395 147.155)"
+          fill="none"
+          stroke={theme.palette.text.primary}
+          stroke-width="2"
+        />
+      </g>
+    </svg>
+  ) : (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32.242" height="18.693" viewBox="0 0 32.242 18.693">
+      <g id="arrow-left" transform="translate(-202.723 -137.717)">
+        <path
+          id="Path_278"
+          data-name="Path 278"
+          d="M16475.775,1222.011l-8.639,8.639,8.639,8.639"
+          transform="translate(-16263 -1083.587)"
+          fill="none"
+          stroke={theme.palette.text.primary}
+          stroke-width="2"
+        />
+        <line
+          id="Line_68"
+          data-name="Line 68"
+          x1="30.57"
+          transform="translate(204.395 147.155)"
+          fill="none"
+          stroke={theme.palette.text.primary}
+          stroke-width="2"
+        />
+      </g>
+    </svg>
+  )}
+
+  <span style={{ marginLeft: '5px' }}>BACK</span> {/* Lägg till 5px mellanrum mellan pilen och texten */}
+</Button>
+
+        <CountryCard
+          country={country}
+          handleBackClick={handleBackClick}
+          handleBorderClick={handleBorderCountryClick}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default CountryPage;
