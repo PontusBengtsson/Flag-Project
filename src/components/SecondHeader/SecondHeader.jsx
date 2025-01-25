f95c08cabd7907afa0fe10b31983e60c708e4c66
PontusBengtsson <pontus.bengtsson@live.com>
2025-01-25 22:42:54 +0100

    Fixed Redundans V6

--- src/components/SecondHeader/SecondHeader.jsx
+++ src/components/SecondHeader/SecondHeader.jsx
@@ -7,39 +7,34 @@ import { useTheme } from '@mui/material/styles';
 const SecondHeader = ({ setRegion, setSearch, isLoading }) => {
   const theme = useTheme(); // Hämta det aktuella temat
 
+  const commonBoxStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    margin: '32px 0px',
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+  };
+
+  const contentBoxStyles = {
+    display: 'flex',
+    flexDirection: { xs: 'column', sm: 'row' }, // Kolumn på små skärmar, rad på större
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: '1150px', // Max bredd för layouten
+  };
+
+  const renderSkeleton = (width, height) => (
+    <Skeleton variant="rectangular" width={width} height={height} />
+  );
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column', // Kolumnlayout för små skärmar (default)
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        margin: '32px 0px',
-        backgroundColor: theme.palette.background.default, // Använd temats bakgrundsfärg
-        color: theme.palette.text.primary, // Använd temats textfärg
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' }, // Kolumn på små skärmar, rad på större
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '100%',
-          maxWidth: '1150px', // Max bredd för layouten
-        }}
-      >
-        {isLoading ? (
-          <Skeleton variant="rectangular" width="370px" height="56px" sx={{ marginBottom: { xs: 2, sm: 0 } }} />
-        ) : (
-          <SearchBar setSearch={setSearch} />
-        )}
-        {isLoading ? (
-          <Skeleton variant="rectangular" width="180px" height="56px" />
-        ) : (
-          <Dropdown setRegion={setRegion} />
-        )}
+    <Box sx={commonBoxStyles}>
+      <Box sx={contentBoxStyles}>
+        {isLoading ? renderSkeleton('370px', '56px') : <SearchBar setSearch={setSearch} />}
+        {isLoading ? renderSkeleton('180px', '56px') : <Dropdown setRegion={setRegion} />}
       </Box>
     </Box>
   );
